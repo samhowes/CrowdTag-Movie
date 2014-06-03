9a926c667bded424c9793df32b2a624a3f02c0ca
Sam Howes <showes06@gmail.com>
2014-06-03 14:17:43 -0400

    Initial Commit Completed Initail Configuration: - Renamed Namespace from "CrowdTag_Movie" to "CrowdTagMovie" - Downloaded Zocial Buttons - Required HTTPS and Login for the App    - Included several AllowAnonymous attributes - Added Google and Facebook Logins - Added Entity framework and enabled migrations

---
+++ CrowdTag-Movie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrowdTagMovie.Startup))]
+namespace CrowdTagMovie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
