c1424b1b138e5920e792df8ae2e24aca4eeb73f6
Sam Howes <showes06@gmail.com>
2014-10-27 19:23:45 -0400

    DrinkDetails

---
+++ CrowdTag-Movie/app/drinks/drinkDetails.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('drinkDetails', drinkDetails);
+
+    drinkDetails.$inject = ['$location']; 
+
+    function drinkDetails($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'drinkDetails';
+
+        activate();
+
+        function activate() { }
+    }
+})();
